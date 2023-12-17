5aab633b445d94fe90355ecb50f8627fa08a4632
Ramindu Nimesh <117975788+ramindu-Nimex@users.noreply.github.com>
2023-12-17 13:28:47 +0530

    completed chat app

--- app/src/main/java/com/example/chat_app/UserAdapter.kt
+++ app/src/main/java/com/example/chat_app/UserAdapter.kt
@@ -1,12 +1,14 @@
 package com.example.chat_app
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 
 class UserAdapter(val context: Context, val userList: ArrayList<User>):
     RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
@@ -24,6 +26,13 @@ class UserAdapter(val context: Context, val userList: ArrayList<User>):
         val currentUser = userList[position]
 
         holder.textName.text = currentUser.name
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context,ChatActivity::class.java)
+            intent.putExtra("name",currentUser.name)
+            intent.putExtra("uid", currentUser.uid)
+            context.startActivity(intent)
+        }
     }
 
     class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
